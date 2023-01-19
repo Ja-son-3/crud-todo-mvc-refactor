77aec520b3eb2b4f582b09dff83307148a4c965d
Jason Huang <jason97521@gmail.com>
2023-01-18 23:36:45 -0500

    get and post refactored

--- server.js
+++ server.js
@@ -5,45 +5,24 @@ const app = express()
 const PORT = 8000
 const mongoose = require("mongoose")
 const connectDB = require("./config/database")
-const TodoTask = require("./models/TodoTask")
+//const TodoTask = require("./models/TodoTask")
+const homeRoutes = require('./routes/home')
+const editRoutes = require('./routes/edit')
 require('dotenv').config({path: './config/.env'})
+
+//Load db module in database.js
+connectDB()
 
 //Set Middleware
 app.set("view engine", "ejs")
 app.use(express.static('public'))
 app.use(express.urlencoded({ extended: true }))
 
-//Load db module in database.js
-connectDB()
+//Set Routes
+app.use('/', homeRoutes)
+//app.use('/edit', editRoutes)
 
-//Get method
-app.get("/", async (req, res) => {
-    try {
-        TodoTask.find({}, (err, tasks) => {
-            res.render("index.ejs", { todoTasks: tasks })
-        })
-    } catch (error) {
-        response.status(500).send({message: error.message})
-    }
-});
-
-//POST method
-app.post('/', async (req, res) => {
-    const todoTask = new TodoTask(
-        {
-            title: req.body.title,
-            content: req.body.content
-        })
-    try {
-        await todoTask.save()
-        console.log(todoTask)
-        res.redirect("/")
-    } catch (error) {
-        if (error) return res.status(500).send(error)
-        res.redirect("/")
-    }
-})
-
+/*
 //Update method
 app
     .route("/edit/:id")
@@ -80,5 +59,5 @@ app
             res.redirect('/')
         })
     })
-
+*/
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
